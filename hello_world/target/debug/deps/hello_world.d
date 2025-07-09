cca5306a5ec022a0a58aeefb9e75ba7e2235b335
Daniil <98248444+kek1v@users.noreply.github.com>
2025-07-09 23:27:22 +0500

    builded a project. ended the 1st chapter of the book

---
+++ hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\dadda\Documents\learn_rs\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+C:\Users\dadda\Documents\learn_rs\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\dadda\Documents\learn_rs\hello_world\target\debug\hello_world.exe: C:\Users\dadda\Documents\learn_rs\hello_world\src\main.rs
