da6467f366bd6721a02f941cfc38b95e51f153ca
Daniil <98248444+kek1v@users.noreply.github.com>
2025-07-10 21:29:11 +0500

    started a gurssing game chapter

---
+++ guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+C:\Users\dadda\Documents\learn_rs\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+
+C:\Users\dadda\Documents\learn_rs\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+
+src\main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\dadda\Documents\learn_rs\guessing_game\target\debug\guessing_game.exe: C:\Users\dadda\Documents\learn_rs\guessing_game\src\main.rs
