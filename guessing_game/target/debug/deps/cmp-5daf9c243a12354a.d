cbe2074bc7495c42478f824bce4b06f09b12fe2a
Daniil <98248444+kek1v@users.noreply.github.com>
2025-07-16 23:23:11 +0500

    added a comparing the guess to the secret number

---
+++ guessing_game/target/debug/deps/cmp-5daf9c243a12354a.d
@@ -0,0 +1,7 @@
+C:\Users\dadda\Documents\learn_rs\guessing_game\target\debug\deps\cmp-5daf9c243a12354a.d: C:\Users\dadda\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cmp-1.0.0\src\lib.rs
+
+C:\Users\dadda\Documents\learn_rs\guessing_game\target\debug\deps\libcmp-5daf9c243a12354a.rlib: C:\Users\dadda\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cmp-1.0.0\src\lib.rs
+
+C:\Users\dadda\Documents\learn_rs\guessing_game\target\debug\deps\libcmp-5daf9c243a12354a.rmeta: C:\Users\dadda\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cmp-1.0.0\src\lib.rs
+
+C:\Users\dadda\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cmp-1.0.0\src\lib.rs:
