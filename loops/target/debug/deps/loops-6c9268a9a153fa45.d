f32fb74f60890ee2f83f4bc6f25de9f896927a30
daniil <daddadov545@gmail.com>
2025-08-21 18:27:40 +0500

    default loop and while

---
+++ loops/target/debug/deps/loops-6c9268a9a153fa45.d
@@ -0,0 +1,5 @@
+C:\Users\dadda\Documents\learn_rs\loops\target\debug\deps\loops-6c9268a9a153fa45.d: src\main.rs
+
+C:\Users\dadda\Documents\learn_rs\loops\target\debug\deps\loops-6c9268a9a153fa45.exe: src\main.rs
+
+src\main.rs:
---
+++ loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+C:\Users\dadda\Documents\learn_rs\loops\target\debug\loops.exe: C:\Users\dadda\Documents\learn_rs\loops\src\main.rs
