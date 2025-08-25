7c08d830eb25355c2386a7f1dcc565f7f246f119
daniil <daddadov545@gmail.com>
2025-08-25 23:32:39 +0500

    string type

---
+++ string/target/debug/deps/string-c5198b9f73df512f.d
@@ -0,0 +1,5 @@
+C:\Users\dadda\Documents\learn_rs\string\target\debug\deps\string-c5198b9f73df512f.d: src\main.rs
+
+C:\Users\dadda\Documents\learn_rs\string\target\debug\deps\string-c5198b9f73df512f.exe: src\main.rs
+
+src\main.rs:
---
+++ string/target/debug/string.d
@@ -0,0 +1,1 @@
+C:\Users\dadda\Documents\learn_rs\string\target\debug\string.exe: C:\Users\dadda\Documents\learn_rs\string\src\main.rs
