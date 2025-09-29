12d7cf20dfee884f32f1e24dbc3521d79e15c40c
kek1v <daddadov545@gmail.com>
2025-09-29 18:42:03 +0500

    struct, ShortHand field's init

--- 5_chapter/structs/src/main.rs
+++ 5_chapter/structs/src/main.rs
@@ -2,16 +2,37 @@ struct User {
     active: bool,
     name: String,
     email: String,
-    age: u8,
+    sign_in_count: u8,
 }
+
+fn build_user(email: String, username: String) -> User {
+    User{
+        active: true,
+        name: username,
+        email: email,
+        sign_in_count: 1,
+    }
+}
+
+fn build_user2(email: String, name: String) -> User{ // with ShortHand field init
+    User{
+        active: true,
+        name, // field and parameters have the same name
+        email,
+        age: 52,
+    }
+}
+
 
 fn main() {
     let mut user1 = User {
         active: true,
         name: String::from("Mike"),
         email: String::from("exmple@email.org"),
-        age: 52,
+        sign_in_count: 52,
     };
 
     user1.email = String::from("Another@email.com");
+
+    let mut user2 = build_user(String::from("u2Email@mail.com"), String::from("user2"));
 }
---
+++ 5_chapter/structs/target/debug/deps/structs-875a99453af825ed.d
@@ -0,0 +1,5 @@
+/home/kekiv/learn_rs/5_chapter/structs/target/debug/deps/structs-875a99453af825ed.d: src/main.rs
+
+/home/kekiv/learn_rs/5_chapter/structs/target/debug/deps/libstructs-875a99453af825ed.rmeta: src/main.rs
+
+src/main.rs:
