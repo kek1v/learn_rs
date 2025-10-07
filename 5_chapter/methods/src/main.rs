8e9ee21f8de439ef6949c417eaf9b310a9a45f3f
kek1v <daddadov545@gmail.com>
2025-10-07 22:34:42 +0500

    methods, associated functions. Chapter 5 is done

--- 5_chapter/methods/src/main.rs
+++ 5_chapter/methods/src/main.rs
@@ -12,6 +12,17 @@ impl Rectangle {
     fn width(&self) -> bool {
         self.width > 0
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -19,11 +30,18 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    let output = rect1.area();
-    println!("The area of the rectangle is {}", output);
+    let sq = Rectangle::square(5);
+    println!("The area of the square with a = 5 is {}", sq.area());
 
-    if rect1.width(){
-        println!("The area of the rectangle is {}", rect1.width);
-    }
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
---
+++ 5_chapter/methods/target/debug/deps/methods-cbd437d59533d077.d
@@ -0,0 +1,5 @@
+/home/kekiv/learn_rs/5_chapter/methods/target/debug/deps/methods-cbd437d59533d077.d: src/main.rs
+
+/home/kekiv/learn_rs/5_chapter/methods/target/debug/deps/libmethods-cbd437d59533d077.rmeta: src/main.rs
+
+src/main.rs:
